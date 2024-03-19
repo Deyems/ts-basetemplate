2b5c4f5c39e10bc7760ce40b612ac84a701360dd
Deyems <adeyemisola11@gmail.com>
2024-03-19 12:10:50 +0100

    added handler for uncaught exception

--- src/index.ts
+++ src/index.ts
@@ -16,5 +16,25 @@ const onCloseSignal = () => {
   setTimeout(() => process.exit(1), 10000).unref(); // Force shutdown after 10s
 };
 
+const onCloseSigTerm = () => {
+  logger.info('sigterm received, shutting down');
+  server.close(() => {
+    logger.info('server closed');
+    process.exit();
+  });
+  setTimeout(() => process.exit(1), 10000).unref(); // Force shutdown after 10s
+};
+
+const onCaughtSignal = () => {
+  logger.info('UNCAUGHT EXCEPTION! 💥 Server Shutting down...');
+  server.close(() => {
+    logger.info('server closed');
+    logger.error('UNCAUGHT EXCEPTION!! 💥 Server Shutting down... ' + new Date(Date.now()));
+    process.exit();
+  });
+  setTimeout(() => process.exit(1), 5000).unref(); // Force shutdown after 5s
+};
+
 process.on('SIGINT', onCloseSignal);
-process.on('SIGTERM', onCloseSignal);
+process.on('SIGTERM', onCloseSigTerm);
+process.on('uncaughtException', onCaughtSignal);
